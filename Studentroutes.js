3ebdaa110a03780077a543c34bda73337f0e27c0
ADITYAARYAN-01 <adityaaryan174@gmail.com>
2025-03-29 03:28:24 +0530

    added an api for undating data based on studentID

--- Studentroutes.js
+++ Studentroutes.js
@@ -2,28 +2,54 @@ const express = require('express');
 
 const router = express.Router();
 
-const Student = require('../MODELorSCHEMA/Student');
+const Student = require('../MODELorSCHEMA/Students');
 
-router.get('/',async(req,res)=>{
-    try{
+router.get('/', async (req, res) => {
+    try {
         const Data = await Student.find();
         console.log("Student data has been Fetched");
         res.status(200).json(Data);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        res.status(500).json({error: "internal server error"});
+        res.status(500).json({ error: "internal server error" });
     }
 })
-router.post('/',async(req,res)=>{
-    try{
+router.post('/', async (req, res) => {
+    try {
         const Data = req.body;
         const NewStudent = new Student(Data);
         const response = await NewStudent.save();
         res.status(200).json(response);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        res.status(500).json({error: "internal server error"});
+        res.status(500).json({ error: "internal server error" });
+    }
+
+})
+router.put('/:STD_ID', async (req, res) => {
+    try {
+        const Studentid = req.params.STD_ID;
+        const updateStudentData = req.body;
+        const response = await Student.findOneAndUpdate({ StudentID: Studentid }, updateStudentData,
+            {
+                new: true,
+                runValidators: true
+            });
+        if (!response) {
+            console.log("not found");
+            res.status.apply(404).json({ error: 'invalid Student id' });
+        }
+        else {
+            console.log("updated");
+            res.status(200).json(response);
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'internal server error' });
     }
 })
+module.exports = router;
+
